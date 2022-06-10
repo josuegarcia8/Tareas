285187d6f09fce623c77bd74bdf700b2af07d550
Josué <Josu@Red-light>
2022-06-09 23:43:48 -0600

    Tarea 2 Josue Garcia

---
+++ src/main/java/com/grupomusical/grupomusical/GrupomusicalApplication.java
@@ -0,0 +1,13 @@
+package com.grupomusical.grupomusical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrupomusicalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrupomusicalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupomusical/grupomusical/GrupomusicalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupomusical.grupomusical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrupomusicalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
